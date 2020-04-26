178d99805ccd71c37816327728160a8a1d1ff49c
bharathbhaskar81 <61418621+bharathbhaskar81@users.noreply.github.com>
2020-04-26 01:52:12 -0400

    Change to prnt

--- src/main/java/com/Simplilearn/demo/DemoApplication.java
+++ src/main/java/com/Simplilearn/demo/DemoApplication.java
@@ -9,7 +9,7 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 		System.out.println("First Java Project Change!!!");
-		System.out.println("Build notification Email Configured!");
+		System.out.println("Build notification Email Configured!!!");
 	}
 
 }
